527a2bd47a2309c5f5f96349762e978991b743e0
jerry3831 <jerryavenz@gmail.com>
2025-04-06 13:01:50 +0200

    python dictionaries

---
+++ basicDataStructures.py
@@ -0,0 +1,28 @@
+# DICTIONARIES 
+# these data structures organize data as key value pairs
+
+numbers = {
+    'one':1,
+    'two':2,
+    'three':3
+}
+
+# looping through dictionaries
+for number, digit in numbers.items():
+    print(f"{number} : {digit}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
